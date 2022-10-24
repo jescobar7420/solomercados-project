fbf6d260efba4ce5da1f00c4377d07d2f961ba94
Javier Escobar <jescobar7420@gmail.com>
2022-10-24 16:19:25 -0300

    Fix query

--- back-end/js/Supermercados_Productos/get_list_product_id.js
+++ back-end/js/Supermercados_Productos/get_list_product_id.js
@@ -4,7 +4,13 @@ const pool = require('../connect_database');
 const GetListSuperProductsId = (req, res) => {
     const id = parseInt(req.params.id);
     let ListSupermercadosProductos = new Array();
-    let query = `SELECT s.supermercado, sp.id_producto, REPLACE(sp.precio_oferta, 'NA', '-') AS precio_oferta, REPLACE(sp.precio_normal, 'NA', '-') AS precio_normal, sp.url_product, sp.fecha, sp.disponibilidad
+    let query = `SELECT s.supermercado, 
+                        sp.id_producto, 
+                        REPLACE(sp.precio_oferta, 'NA', '-') AS precio_oferta, 
+                        REPLACE(sp.precio_normal, 'NA', '-') AS precio_normal, 
+                        sp.url_product, 
+                        sp.fecha, 
+                        REPLACE(sp.disponibilidad, 'Yes', 'Sí') AS disponibilidad
                  FROM supermercados_productos AS sp
                  JOIN supermercados AS s ON s.id_supermercado = sp.id_supermercado
                  WHERE sp.id_producto = $1`;
